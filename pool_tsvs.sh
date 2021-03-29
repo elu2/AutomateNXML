5d362542680756ec18119623b2a6d95af69d25c6
elu2 <60558250+elu2@users.noreply.github.com>
2021-03-29 12:43:43 -0700

    Added pooling script.

---
+++ pool_tsvs.sh
@@ -0,0 +1,11 @@
+# Pools all tsv files into a papers_as_tsv directory
+# Need to manually make year's pool directory before running.
+
+year=2019
+for month in {1..12}
+do
+
+cd /xdisk/guangyao/REACH2/mk_jars/all_fat_jars/${year}_${month}/output/
+cp *-arizona-out.tsv /xdisk/guangyao/REACH2/mk_jars/pools/${year}
+
+done

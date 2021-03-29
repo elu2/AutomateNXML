d5b9a6238e17b3f5d9e77a594899e7e677f704ec
elu2 <60558250+elu2@users.noreply.github.com>
2021-03-29 12:51:52 -0700

    Incorporated pool_tsvs.sh.

--- meta_script.sh
+++ meta_script.sh
@@ -8,4 +8,4 @@ sed -i "s/year=.*/year=${year}/g" nxml_runner.sh
 sed -i "s/year=.*/year=${year}/g" remove_from_papers.sh
 sed -i "s/year=.*/year=${year}/g" remove_postrun.sh
 sed -i "s/year=.*/year=${year}/g" sbatch_runner.sh
-
+sed -i "s/year=.*/year=${year}/g" pool_tsvs.sh

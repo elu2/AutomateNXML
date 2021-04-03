4b436a6efa241a278cc1e32ddd96fd47106aabb9
elu2 <60558250+elu2@users.noreply.github.com>
2021-04-03 13:01:20 -0700

    Update pool_tsvs.sh

--- pool_tsvs.sh
+++ pool_tsvs.sh
@@ -6,6 +6,6 @@ for month in {1..12}
 do
 
 cd /xdisk/guangyao/REACH2/mk_jars/all_fat_jars/${year}_${month}/output/
-cp *-arizona-out.tsv /xdisk/guangyao/REACH2/mk_jars/pools/${year}
+cp *-arizona-out.tsv /xdisk/guangyao/REACH2/Warehouse/paper_pools/${year}
 
 done
